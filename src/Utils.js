1e10a70e964100b2fd962a913d488d5b1a0534ac
hemny-singh <mail.hemny@gmail.com>
2017-02-27 01:29:04 +0530

    Added chart for contribution for team

--- src/Utils.js
+++ src/Utils.js
@@ -70,8 +70,38 @@ export let getDataForChartPerYear = (data, property) => {
     processedData.year.push(key)
     processedData.data.push(data[key][property])
   })
-  console.log(processedData)
   return processedData
+}
+
+export let contributionChartData = (data) => {
+  let groupedCalculatedData = {}, date, score
+
+  data.map((matchScore) => {
+    date = new Date(matchScore.date)
+    date = date.getFullYear()
+    if (!groupedCalculatedData.hasOwnProperty(date)) {
+      groupedCalculatedData[date] = {
+        scoredButLost: 0,
+        scoredAndWon: 0,
+        didNotScoreAndLost: 0
+      }
+    }
+    if (matchScore.batting_score === 'TDNB' || matchScore.batting_score === 'DNB' || 
+      matchScore.batting_score === '-') {
+    } else {
+      score = matchScore.batting_score.split('*')
+      score = parseInt(score[0], 10)
+
+      if (matchScore.match_result === 'won' && score >= 50) {
+        groupedCalculatedData[date].scoredAndWon++
+      } else if (matchScore.match_result === 'lost' && score < 35) {
+        groupedCalculatedData[date].didNotScoreAndLost++
+      } else if (matchScore.match_result === 'lost' && score >= 50) {
+        groupedCalculatedData[date].scoredButLost++
+      }
+    }
+  })
+  return groupedCalculatedData
 }
 
 export let generateInitialConfig = () => {
@@ -83,8 +113,15 @@ export let generateInitialConfig = () => {
       text: ''
     },
     xAxis: {
+      title: {
+        text: ''
+      },
       categories: []
     },
+    chart: {
+      type: 'line'
+    },
+    colors: ['#398dc6', '#f26d7d', '#1c9a71', '#b34ab2', '#f39448', '#0051a2', '#5bc0de'],
     yAxis: {
       title: {
         text: ''
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,33 +1,156 @@
 import React, {Component} from 'react'
 import Chart from './ChartComponent'
-import { generateInitialConfig, processDataPerYear, getDataForChartPerYear } from './../Utils'
+import { generateInitialConfig, processDataPerYear, getDataForChartPerYear,
+  contributionChartData } from './../Utils'
 import { SACHIN_DATA } from './../data/sachinDetails'
 
 export default class Dashboard extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      totalRunChartConfig: generateInitialConfig(),
-      averageChartConfig: generateInitialConfig(),
-      highScoredChartConfig: generateInitialConfig()
+      totalRunChartConfig: {},
+      averageChartConfig: {},
+      highScoredChartConfig: {},
+      contributionStatisticsConfig: {}
     }
   }
 
+
+  //Helper function to format data as per highchart configuration for totalRun
+  getTotalRunChartData = (data) => {
+    let processedData = {
+      categories: [],
+      series: [{name: 'Batting Score', data: []}]
+    }
+
+    Object.keys(data).map((key) => {
+      processedData.categories.push(key)
+      processedData.series[0].data.push(data[key]['totalRun'])
+    })
+    return processedData
+  }
+
+  //Helper function to format data as per highchart configuration for Average Rate
+  getAverageChartData = (data) => {
+    let processedData = {
+      categories: [],
+      series: [{name: 'Avearge Rate', data: []}]
+    }, averageScore = 0
+
+    Object.keys(data).map((key) => {
+      processedData.categories.push(key)
+      averageScore = data[key]['totalRun'] / (data[key]['totalMatchCount'] - data[key]['notOutCount'])
+      processedData.series[0].data.push(averageScore)
+    })
+    return processedData
+  }
+
+  //Helper function to format data as per highchart configuration for High 
+  //Score i.e. Centuries and halfCenturies  
+  getHighScoreChartData = (data) => {
+    let processedData = {
+      categories: [],
+      series: [
+        {
+          name: 'Half Centuries',
+          data: []
+        }, {
+          name: 'Centuries',
+          data: []
+        }
+      ]
+    }
+
+    Object.keys(data).map((key) => {
+      processedData.categories.push(key)
+      processedData.series[0].data.push(data[key].halfCenturies)
+      processedData.series[1].data.push(data[key].centuries)
+    })
+    return processedData
+  }
+
+  getContributionStatisticsChartData = (data) => {
+    let processedData = {
+      categories: [],
+      series: [
+        {
+          name: 'Scored and Team Won',
+          data: []
+        }, {
+          name: 'Did not Score and Team Lost',
+          data: []
+        }, {
+          name: 'Scored but Lost',
+          data: []
+        }
+      ]
+    }
+
+    Object.keys(data).map((key) => {
+      processedData.categories.push(key)
+      processedData.series[0].data.push(data[key].scoredAndWon)
+      processedData.series[1].data.push(data[key].didNotScoreAndLost)
+      processedData.series[2].data.push(data[key].scoredButLost)
+    })
+    return processedData
+  }
+
+  //Generic funtion to set chart configuration and set the sate
+  setChartConfigState = (config) => {
+    let tempConfig = {}
+    Object.keys(config).map((key) => {
+      tempConfig = generateInitialConfig()
+      tempConfig.chart.type = config[key].chartType || 'line'
+      tempConfig.title.text = config[key].title
+      tempConfig.xAxis.title.text = config[key].xAxisTitle
+      tempConfig.yAxis.title.text = config[key].yAxisTitle
+      tempConfig.xAxis.categories = config[key].data.categories,
+      tempConfig.series = config[key].data.series
+      this.setState({[key]: tempConfig})
+    })
+  }
+
+
   componentDidMount () {
     let calculatedData = processDataPerYear(SACHIN_DATA),
-      chartData = getDataForChartPerYear(calculatedData, 'totalRun'),
-      config = JSON.parse(JSON.stringify(this.state.totalRunChartConfig))
-
-      config.xAxis.categories = chartData.year
-      config.series.push({data: chartData.data})
-
-    this.setState({totalRunChartConfig: config})
+      contributionStatistics = contributionChartData(SACHIN_DATA),
+      chartConfigurations = {
+        totalRunChartConfig: {
+          title: 'Total Batting Score vs Year',
+          yAxisTitle: 'Batting Score',
+          xAxisTitle: 'Year',
+          data: this.getTotalRunChartData(calculatedData)
+        }, 
+        averageChartConfig: {
+          title: 'Average Rate vs Year',
+          yAxisTitle: 'Average Rate',
+          xAxisTitle: 'Year',
+          data: this.getAverageChartData(calculatedData)
+        },
+        highScoredChartConfig: {
+          title: 'Centuries & Half Centuries vs Year',
+          yAxisTitle: 'Batting Score',
+          xAxisTitle: 'Year',
+          data: this.getHighScoreChartData(calculatedData)
+        },
+        contributionStatisticsConfig: {
+          title: 'Cotribution Statistics vs Year',
+          yAxisTitle: 'Count',
+          xAxisTitle: 'Year',
+          data: this.getContributionStatisticsChartData(contributionStatistics),
+          chartType: 'column'
+        }
+      }
+    this.setChartConfigState(chartConfigurations)
   }
 
   render () {
     return (
       <div>
         <Chart config={this.state.totalRunChartConfig}/>
+        <Chart config={this.state.averageChartConfig}/>
+        <Chart config={this.state.highScoredChartConfig}/>
+        <Chart config={this.state.contributionStatisticsConfig}/>
       </div>
     )
   }
